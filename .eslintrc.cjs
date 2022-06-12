8b3e6dd5df1952a45cda29190405b09a5ccaff2d
flec <mfuellemann@gmail.com>
2022-06-12 12:13:38 +0200

    setup jest and eslint

---
+++ .eslintrc.cjs
@@ -0,0 +1,25 @@
+const { join } = require("path");
+
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2019,
+    project: join(__dirname, "./tsconfig.json"),
+    sourceType: "module"
+  },
+  plugins: [
+    '@typescript-eslint',
+    'jest',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:rxjs/recommended',
+  ],
+  rules: {
+    semi: ["error", "always"],
+    quotes: ["error", "single"],
+  }
+};
